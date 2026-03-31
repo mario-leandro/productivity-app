0c33b26247229ac359540034ff3adba106ac32d4
mario-leandro <marioleandro2004@gmail.com>
2026-03-31 10:00:33 -0300

    D

---
+++ app/diario/nova.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function NovoDiario() {
+  return (
+    <View>
+      <Text>Novo Diario</Text>
+    </View>
+  );
+}
---
+++ app/tarefas/[id].tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Tarefas() {
+  return (
+    <View>
+      <Text>Tarefas</Text>
+    </View>
+  );
+}

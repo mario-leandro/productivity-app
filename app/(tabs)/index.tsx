e934085fb7d9728a1214da4e06aa2c5a56d55db6
mario-leandro <marioleandro2004@gmail.com>
2026-04-08 18:41:03 -0300

    ajustando tela principal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const arrOptions = [
+    { id: 1, name: "Lista de Tarefas" },
+    { id: 2, name: "Foco" },
+    { id: 3, name: "Diário" },
+  ];
+
   return (
     <View style={styles.container}>
       <View style={styles.apresentacao}>
@@ -8,7 +14,17 @@ export default function Index() {
       </View>
 
       <View style={styles.content}>
-        <Text>Qual das opções vai utilizar agora?</Text>
+        <View>
+          <Text style={styles.title}>Qual das opções vai utilizar agora?</Text>
+        </View>
+
+        <View style={styles.options}>
+          {arrOptions.map((option) => (
+            <View style={styles.card} key={option.id}>
+              <Text>{option.name}</Text>
+            </View>
+          ))}
+        </View>
       </View>
     </View>
   );
@@ -19,17 +35,38 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   apresentacao: {
-    width: 100,
-    height: 60,
-    // backgroundColor: "#eee",
+    width: "100%",
+    height: 80,
+    paddingHorizontal: 16,
+    paddingVertical: 18,
   },
   apresentacaoText: {
-    fontSize: 24,
-    padding: 10,
+    fontSize: 30,
   },
   content: {
-    width: 100,
-    height: 80,
-    backgroundColor: "#ccc",
+    width: "100%",
+    // height: "100%",
+    // backgroundColor: "#ccc",
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  options: {
+    width: "100%",
+    height: 200,
+    // backgroundColor: "#ddd",
+    marginTop: 12,
+    padding: 8,
+  },
+  card: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#eee",
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
